2ecb21afab2f02249f55c117794555369c6f20d2
RaeHwan Park <prh0108@naver.com>
2025-03-18 13:56:40 +0900

    기본 아키텍처 연결

---
+++ src/main/java/com/example/capstone/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.capstone.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String index() {
+        return "index";
+    }
+}
